b7b1af948559ea734ceb76dda9589723dc1bfc04
Markus Schubert <markus@toto.io>
2026-07-06 17:02:38 +0200

    Release v0.11.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Scenex.MixProject do
   def project do
     [
       app: :scenex,
-      version: "0.11.0-dev",
+      version: "0.11.0",
       elixir: "~> 1.15",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
